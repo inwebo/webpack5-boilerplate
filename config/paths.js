8d2d62793212068df62c3e32279f8d4ffe6b6196
Inwebo <github@inwebo.net>
2022-06-18 11:31:11 +0200

    Add docs

--- config/paths.js
+++ config/paths.js
@@ -7,6 +7,9 @@ module.exports = {
   // Production build files
   build: path.resolve(__dirname, '../dist'),
 
+  // Documentation
+  docs: path.resolve(__dirname, '../docs'),
+
   // Static files that get copied to build folder
   public: path.resolve(__dirname, '../public'),
 }
